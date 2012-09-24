bb521ad53f443fa8d45873fe54c12de5f9d148ea
markrendle <mark@markrendle.net>
2012-09-24 15:08:47 +0100

    Version 0

---
+++ moist.js
@@ -0,0 +1,101 @@
+;(function() {
+    var voidTags = ['area','base','br','col','command','embed','hr','img','input','link','meta','param','source'];
+    function isVoid(tag) {
+        return voidTags.indexOf(tag.toLowerCase()) > -1;
+    }
+    function typeOf(value) {
+        var s = typeof value;
+        if (s === 'object') {
+            if (value) {
+                if (value instanceof Array) {
+                    s = 'array';
+                }
+            } else {
+                s = 'null';
+            }
+        }
+        return s;
+    }
+    function arrayToHtml(array) {
+        var html = '';
+        for (var i = 0; i < array.length; i++) {
+            switch (typeOf(array[i])) {
+                case "string":
+                case "number":
+                    html = html + array[i];
+                    break;
+                case "object":
+                    html = html + toHtml(array[i]);
+                    break;
+                case "array":
+                    html = html + arrayToHtml(array[i]);
+                    break;
+            }
+        };
+        return html;
+    }
+    function stringToTag(str) {
+        return str.indexOf('_') < 0 ? str
+            :
+            str.replace(/^_+/, '')
+                .replace(/_+$/, '')
+                .replace(/_/, '-');
+    }
+    function objectToHtml(object) {
+        var tag = "",
+            html = "",
+            attributes = "";
+        for (var key in object) {
+            if (object.hasOwnProperty(key)) {
+                var value = object[key];
+                switch (typeOf(value)) {
+                    case "array":
+                        tag = stringToTag(key);
+                        break;
+                    case "string":
+                    case "number":
+                        attributes = attributes + ' ' + stringToTag(key) + '="' + value + '"';
+                        break;
+                    case "boolean":
+                        attributes = attributes + ' ' + stringToTag(key) + '="' + (value ? 'true' : 'false') + '"';
+                        break;
+                    case "null":
+                        attributes = attributes + ' ' + stringToTag(key);
+                        break;
+                }
+            }
+        }
+        if (tag.length > 0) {
+            html = '<' + tag + attributes + '>';
+            if (object[tag].length > 0) {
+                html = html + arrayToHtml(object[tag]);
+            }
+            if (!isVoid(tag)) {
+                html = html + '</' + tag + '>';
+            }
+            return html;
+        }
+        throw new Error("Fail");
+    }
+    function toHtml(object) {
+        switch (typeOf(object)) {
+            case "object":
+                return objectToHtml(object);
+                break;
+            case "array":
+                return arrayToHtml(object);
+                break;
+            default:
+                return object;
+        };
+    }
+    var moist = {html: toHtml}
+    if (typeof window !== "undefined") {
+        window.Moist = moist;
+    } else {
+        if (typeof exports !== "undefined") {
+            exports.html = toHtml;
+        }
+    }
+    return moist;
+})();
---
+++ test/test.js
@@ -0,0 +1,60 @@
+var assert = require("assert");
+var moist = require("../moist.js");
+
+describe("Moist", function() {
+    describe("#html(obj)", function() {
+
+        it("should generate a div", function() {
+            assert.equal('<div></div>', moist.html({div: []}));
+        });
+
+        it("should generate a div with class", function() {
+            var object = {div: [], _class: "content"},
+                expected = '<div class="content"></div>',
+                actual = moist.html(object);
+            assert.equal(actual, expected);
+        });
+
+        it("should generate a div with class and content", function() {
+            var object = {div: ["bobbins"], _class: "content"},
+                expected = '<div class="content">bobbins</div>',
+                actual = moist.html(object);
+            assert.equal(actual, expected);
+        });
+
+        it("should generate a div with class and nested content", function() {
+            var object = {div: [{h1: ["bobbins"]}], _class: "content"},
+                expected = '<div class="content"><h1>bobbins</h1></div>',
+                actual = moist.html(object);
+            assert.equal(actual, expected);
+        });
+
+        it("should generate a div with class and nested contents", function() {
+            var object = {div: [{h1: ["bobbins"]}, {p: ["nonsense"]}], _class: "content"},
+                expected = '<div class="content"><h1>bobbins</h1><p>nonsense</p></div>',
+                actual = moist.html(object);
+            assert.equal(actual, expected);
+        });
+
+        it("should generate two divs", function() {
+            var object = [{div:[], _class: "span3"},{div: [], _class: "span9"}],
+                expected = '<div class="span3"></div><div class="span9"></div>',
+                actual = moist.html(object);
+            assert.equal(actual, expected);
+        });
+
+        it("should not generate a closing tag for img", function() {
+            var object = {img: [], src: "foo.png"},
+                expected = '<img src="foo.png">',
+                actual = moist.html(object);
+            assert.equal(actual, expected);
+        });
+
+        it("should generate a div with ng-view", function() {
+            var object = {div: [], ng_view: null},
+                expected = '<div ng-view></div>',
+                actual = moist.html(object);
+            assert.equal(actual, expected);
+        }); 
+    });
+});
